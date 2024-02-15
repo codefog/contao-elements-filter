847725b2db1825c0b5d09e43e7a77442eae79b50
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2024-02-15 08:37:04 +0100

    Bump compatibility to Contao 5.3 LTS

--- src/CodefogElementsFilterBundle.php
+++ src/CodefogElementsFilterBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CodefogElementsFilterBundle extends Bundle
 {
-    public function getPath()
+    public function getPath(): string
     {
         return \dirname(__DIR__);
     }
